fe9757b20cf01973cc76d3200d5cc893e5bcd395
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-13 22:16:27 -0300

    (dtmoney): create new transacion feature

--- aulas/dtmoney/src/index.tsx
+++ aulas/dtmoney/src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+	models: {
+		transaction: Model,
+	},
+
+	routes() {
+		this.namespace = 'api';
+
+		this.get('/transactions', () => {
+			return this.schema.all('transaction');
+		});
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+			return schema.create('transaction', data);
+		});
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
