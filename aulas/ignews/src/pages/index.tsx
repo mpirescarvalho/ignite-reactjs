7123e0823c0a313606bac680640052661b8a32a2
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-20 22:32:24 -0300

    (ignews): retrive subscription price from stripe

--- aulas/ignews/src/pages/index.tsx
+++ aulas/ignews/src/pages/index.tsx
@@ -1,13 +1,19 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from '../styles/Home.module.scss';
 
-export default function Home(props) {
-	console.log(props);
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	};
+}
 
+export default function Home({ product }: HomeProps) {
 	return (
 		<>
 			<Head>
@@ -22,9 +28,9 @@ export default function Home(props) {
 					</h1>
 					<p>
 						Get access to all the publications <br />
-						<span>for $9.90 month</span>
+						<span>for {product.amount} month</span>
 					</p>
-					<SubscribeButton />
+					<SubscribeButton priceId={product.priceId} />
 				</section>
 
 				<img src="/images/avatar.svg" alt="Girl coding" />
@@ -32,3 +38,22 @@ export default function Home(props) {
 		</>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve('price_1JFTRgHJxyePIZElOlcaMkKG');
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		}).format(price.unit_amount / 100),
+	};
+
+	return {
+		props: {
+			product,
+		},
+		revalidate: 60 * 60 * 24, // 24 hours
+	};
+};
