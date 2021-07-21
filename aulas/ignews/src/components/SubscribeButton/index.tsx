e76ed87991b0a43306050992fac5f70ba0574a30
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-20 22:04:06 -0300

    (ignews): add home page structure

---
+++ aulas/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButton() {
+	return (
+		<button type="button" className={styles.subscriveButton}>
+			Subscribe now
+		</button>
+	);
+}
--- aulas/ignews/src/pages/index.tsx
+++ aulas/ignews/src/pages/index.tsx
@@ -1,12 +1,34 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from '../styles/Home.module.scss';
+
+export default function Home(props) {
+	console.log(props);
+
 	return (
 		<>
 			<Head>
-				<title>Início | ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
-			<h1>Hello World</h1>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for $9.90 month</span>
+					</p>
+					<SubscribeButton />
+				</section>
+
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
 		</>
 	);
 }
