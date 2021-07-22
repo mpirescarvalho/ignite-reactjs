b3aef3ec3f3b371a04393c6c0894641746f9e831
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-22 20:27:21 -0300

    (ignews): add subscription using stripe

--- aulas/ignews/src/components/SubscribeButton/index.tsx
+++ aulas/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/client';
+import { getStripeJs } from '../../services/stripe-js';
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +9,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+	const [session] = useSession();
+
+	async function handleSubscribe() {
+		if (!session) {
+			signIn('github');
+			return;
+		}
+
+		try {
+			const response = await api.post('/subscribe');
+			const { sessionId } = response.data;
+			const stripe = await getStripeJs();
+			await stripe.redirectToCheckout({ sessionId });
+		} catch (err) {
+			alert(err.message);
+		}
+	}
+
 	return (
-		<button type="button" className={styles.subscriveButton}>
+		<button
+			type="button"
+			className={styles.subscribeButton}
+			onClick={handleSubscribe}
+		>
 			Subscribe now
 		</button>
 	);
