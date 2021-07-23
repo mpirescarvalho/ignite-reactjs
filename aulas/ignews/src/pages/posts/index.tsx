0cbcf2bd85eea20ccaf79dcff9d31af431f5eb5c
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-23 20:43:34 -0300

    (ignews): add post page

--- aulas/ignews/src/pages/posts/index.tsx
+++ aulas/ignews/src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
 			<main className={styles.container}>
 				<div className={styles.posts}>
 					{posts.map((post) => (
-						<a key={post.slug} href="#">
-							<time>{post.updatedAt}</time>
-							<strong>{post.title}</strong>
-							<p>{post.excerpt}</p>
-						</a>
+						<Link key={post.slug} href={`/posts/${post.slug}`}>
+							<a href="#">
+								<time>{post.updatedAt}</time>
+								<strong>{post.title}</strong>
+								<p>{post.excerpt}</p>
+							</a>
+						</Link>
 					))}
 				</div>
 			</main>
