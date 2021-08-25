6711b69f15559ad454d6dcd2d658a59c0ddd75a7
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-08-25 20:42:20 -0300

    (dashgo): add responsivity to sidebar

--- aulas/dashgo/src/pages/_app.tsx
+++ aulas/dashgo/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
+			<SidebarDrawerProvider>
+				<Component {...pageProps} />
+			</SidebarDrawerProvider>
 		</ChakraProvider>
 	);
 }
