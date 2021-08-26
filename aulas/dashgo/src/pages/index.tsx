29c5dfafd008afa53af40d9995ab5971010eb077
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-08-25 22:02:56 -0300

    (dashgo): add validation to signin form

--- aulas/dashgo/src/pages/index.tsx
+++ aulas/dashgo/src/pages/index.tsx
@@ -1,8 +1,30 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+	email: string;
+	password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+	email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+	password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn() {
+	const { register, handleSubmit, formState } = useForm({
+		resolver: yupResolver(signInFormSchema),
+	});
+
+	const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		console.log(data);
+	};
+
 	return (
 		<Flex w="100vw" h="100vh" align="center" justify="center">
 			<Flex
@@ -13,13 +35,32 @@ export default function SignIn() {
 				p="8"
 				borderRadius={8}
 				flexDir="column"
+				onSubmit={handleSubmit(handleSignIn)}
 			>
 				<Stack spacing="4">
-					<Input name="email" type="email" label="E-mail" />
-					<Input name="password" type="password" label="Senha" />
+					<Input
+						name="email"
+						type="email"
+						label="E-mail"
+						error={formState.errors.email}
+						{...register('email')}
+					/>
+					<Input
+						name="password"
+						type="password"
+						label="Senha"
+						error={formState.errors.password}
+						{...register('password')}
+					/>
 				</Stack>
 
-				<Button type="submit" mt="6" colorScheme="pink" size="lg">
+				<Button
+					type="submit"
+					mt="6"
+					colorScheme="pink"
+					size="lg"
+					isLoading={formState.isSubmitting}
+				>
 					Entrar
 				</Button>
 			</Flex>
