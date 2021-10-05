1fca21cff47b4b710e959601516cd2620e943c75
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-10-05 12:11:41 -0300

    (dashgo): add users pagination

--- aulas/dashgo/src/pages/users/index.tsx
+++ aulas/dashgo/src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 	Spinner,
 } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
@@ -24,7 +25,8 @@ import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-	const { data, isLoading, error, isFetching } = useUsers();
+	const [page, setPage] = useState(1);
+	const { data, isLoading, error, isFetching } = useUsers(page);
 
 	const isWideVersion = useBreakpointValue({
 		base: false,
@@ -82,7 +84,7 @@ export default function UserList() {
 									</Tr>
 								</Thead>
 								<Tbody>
-									{data.map((user) => (
+									{data.users.map((user) => (
 										<Tr key={user.id}>
 											<Td px={['4', '4', '6']}>
 												<Checkbox colorScheme="pink" />
@@ -114,7 +116,11 @@ export default function UserList() {
 								</Tbody>
 							</Table>
 
-							<Pagination />
+							<Pagination
+								totalCountOfRegisters={data.totalCount}
+								currentPage={page}
+								onPageChange={setPage}
+							/>
 						</>
 					)}
 				</Box>
